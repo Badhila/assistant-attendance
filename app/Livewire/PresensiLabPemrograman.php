71602478b3d02d06a75a34018560fce7a75e6597
presidenwashil <raihanpresiden2003@gmail.com>
2024-07-25 14:45:13 +0800

    add docs for readable code

--- app/Livewire/PresensiLabPemrograman.php
+++ app/Livewire/PresensiLabPemrograman.php
@@ -11,6 +11,12 @@ use App\View\Components\AppLayout;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
 
+/**
+ * Class PresensiLabPemrograman
+ * @package App\Livewire
+ *
+ * This class represents the Livewire component for managing the attendance in the Pemrograman lab.
+ */
 class PresensiLabPemrograman extends Component
 {
     public $rfid = '';
@@ -23,12 +29,25 @@ class PresensiLabPemrograman extends Component
 
     protected $listeners = ['refreshRfid'];
 
+    /**
+     * Mount the component.
+     *
+     * This method is called when the component is being mounted.
+     * It updates the RFID value and resets the component's values.
+     */
     public function mount()
     {
         $this->updateRfid();
     }
 
     #[Computed()]
+    /**
+     * Get the current schedule.
+     *
+     * This method retrieves the current schedule for the Pemrograman lab.
+     *
+     * @return mixed The current schedule.
+     */
     public function schedule()
     {
         return Schedule::with('group', 'period', 'period.day', 'room')
@@ -46,6 +65,13 @@ class PresensiLabPemrograman extends Component
     }
 
     #[Computed()]
+    /**
+     * Get the assistants for the current meet.
+     *
+     * This method retrieves the assistants for the current meet in the Pemrograman lab.
+     *
+     * @return mixed The assistants for the current meet.
+     */
     public function assistants()
     {
         return AssistantMeet::with('assistant','meet', 'meet.period', 'meet.room')
@@ -62,15 +88,31 @@ class PresensiLabPemrograman extends Component
             ->get();
     }
 
+    /**
+     * Update the RFID value.
+     *
+     * This method updates the RFID value and performs various checks and operations based on the RFID value.
+     */
+    /**
+     * Update the RFID information and perform attendance tracking.
+     *
+     * @return void
+     */
     public function updateRfid()
     {
         $this->resetValues();
 
+        // Get the latest RFID entry
         $latestRfid = Rfid::latest()->first();
+
+        // Get the current day and time
         $currentDay = now()->format('l');
         $currentTime = now()->format('H:i:s');
+
+        // Set the room name
         $room = 'Pemrograman';
 
+        // Find the ongoing period for the given room, day, and time
         $ongoingPeriod = Schedule::with('group', 'period', 'period.day', 'room')
             ->whereHas('room', function ($query) use ($room) {
                 $query->where('name', $room);
@@ -84,32 +126,40 @@ class PresensiLabPemrograman extends Component
             })
             ->first();
 
+        // If there is no ongoing period, set the necessary variables and return
         if (!$ongoingPeriod) {
             $this->ongoingPeriod = null;
             $this->rfidExists = true;
             return;
         }
 
+        // If there is no latest RFID entry, truncate the Rfid table and return
         if (!$latestRfid) {
             Rfid::truncate();
             return;
         }
 
+        // Set the RFID value
         $this->rfid = $latestRfid->rfid;
 
+        // Set the ongoing period
         $this->ongoingPeriod = $ongoingPeriod;
 
+        // Count the total number of meets for the ongoing period
         $totalMeetCount = Meet::where('group_id', $ongoingPeriod->group_id)
             ->where('period_id', $ongoingPeriod->period_id)
             ->where('room_id', $ongoingPeriod->room_id)
             ->count('meet_count');
 
+        // Calculate the new meet count
         $newMeetCount = $totalMeetCount + 1;
 
+        // If there are no previous meets, set the new meet count to 1
         if (!$totalMeetCount) {
             $newMeetCount = 1;
         }
 
+        // Create or update the meet entry
         $meet = Meet::firstOrCreate([
             'group_id' => $ongoingPeriod->group_id,
             'period_id' => $ongoingPeriod->period_id,
@@ -119,8 +169,10 @@ class PresensiLabPemrograman extends Component
             'meet_count' => $newMeetCount
         ]);
 
+        // Find the assistant based on the RFID value
         $assistant = Assistant::where('rfid', $this->rfid)->first();
 
+        // If the assistant is not found, set the necessary variables, truncate the Rfid table, and return
         if (!$assistant) {
             $this->rfidExists = false;
             $this->name = null;
@@ -128,19 +180,23 @@ class PresensiLabPemrograman extends Component
             return;
         }
 
+        // Set the necessary variables for the assistant
         $this->rfidExists = true;
         $this->name = $assistant->name;
 
+        // Check if the assistant has already attended the current meet
         $alreadyAttended = AssistantMeet::where('meet_id', $meet->id)
             ->where('assistant_id', $assistant->id)
             ->first();
 
+        // If the assistant has already attended, set the necessary variables, truncate the Rfid table, and return
         if ($alreadyAttended) {
             $this->hasAttended = true;
             Rfid::truncate();
             return;
         }
 
+        // Check if the assistant has already attended a meet in another room on the same day
         $alreadyAttendOnOtherRoom = AssistantMeet::where('assistant_id', $assistant->id)
             ->whereHas('meet', function ($query) use ($ongoingPeriod) {
                 $query->where('date', now()->format('Y-m-d'))
@@ -148,6 +204,7 @@ class PresensiLabPemrograman extends Component
             })
             ->first();
 
+        // If the assistant has already attended in another room, set the necessary variables, truncate the Rfid table, and return
         if ($alreadyAttendOnOtherRoom) {
             $this->rfidExists = true;
             $this->alreadyAttendOnOtherRoom = true;
@@ -155,6 +212,7 @@ class PresensiLabPemrograman extends Component
             return;
         }
 
+        // Check if the room is already full
         $roomSlot = AssistantMeet::where('meet_id', $meet->id)->sum('slot_used');
         if ($roomSlot >= $ongoingPeriod->room->slots) {
             $this->roomFull = true;
@@ -164,6 +222,7 @@ class PresensiLabPemrograman extends Component
             return;
         }
 
+        // If the assistant has not already attended, create a new AssistantMeet entry
         if (!$alreadyAttended) {
             $this->hasAttended = false;
 
@@ -176,9 +235,15 @@ class PresensiLabPemrograman extends Component
             $this->hasAttended = true;
         }
 
+        // Truncate the Rfid table
         Rfid::truncate();
     }
 
+    /**
+     * Reset the component's values.
+     *
+     * This method resets the component's values to their initial state.
+     */
     public function resetValues()
     {
         $this->rfid = '';
@@ -189,6 +254,13 @@ class PresensiLabPemrograman extends Component
         $this->ongoingPeriod = null;
     }
 
+    /**
+     * Render the component.
+     *
+     * This method renders the component's view.
+     *
+     * @return \Illuminate\Contracts\View\View The rendered view.
+     */
     public function render()
     {
         return view('livewire.presensi-lab-pemrograman')
